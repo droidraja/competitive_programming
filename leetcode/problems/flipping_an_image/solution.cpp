63947438ead5284f01156ab02bdce342eca4ef35
Raja <srajasudhan@gmail.com>
2021-12-08 11:58:52 +0000

    Sync LeetCode submission - Flipping an Image (cpp)

--- leetcode/problems/flipping_an_image/solution.cpp
+++ leetcode/problems/flipping_an_image/solution.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& A) {
-        int rows = A.size();
-        if(rows==0) return A;
-        int cols = A[0].size();
-        if(cols==0) return A;
-        
-        //first flip the image 
-        for(int i=0;i<rows;i++) {
-            for(int j=0;j<cols/2;j++) {
-                int temp = A[i][j];
-                A[i][j] = A[i][cols-1-j];
-                A[i][cols-1-j] = temp;
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+        vector<vector<int>> a;
+        for(vector<int> row: image ) {
+            
+            for(int i=0;i<row.size()/2;i++) {
+                int temp = row[i];
+                row[i] = row[row.size()-1-i];
+                row[row.size()-1-i] = temp;
+                row[i] ^= 1;
+                row[row.size()-1-i] ^= 1;
             }
-        }
-        
-        //invert the image
-        for(int i=0;i<rows;i++) {
-            for(int j=0;j<cols;j++) {
-                A[i][j] = (A[i][j]==0)?1:0;
+            if(row.size()%2==1) {
+                row[row.size()/2] ^= 1;
             }
+            a.push_back(row);
+            
         }
-        
-        return A;
+        return a;
     }
 };

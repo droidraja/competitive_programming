3be33ab09d4260c49718d39477e41b53c5265506
Raja <srajasudhan@gmail.com>
2023-04-09 04:11:06 +0000

    Sync LeetCode submission - Hamming Distance (cpp)

--- leetcode/problems/hamming_distance/solution.cpp
+++ leetcode/problems/hamming_distance/solution.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
-    int countSetBits(int number) {
-        int setBits = 0;
-        while(number!=0) {
-            if(number & 1) setBits++;
-            number = number>>1;
-        }
-        return setBits;
-    }
     int hammingDistance(int x, int y) {
-        //doing xor of two ints sets the bits for which x and y bits are diff
-        int different_bits = x^y;
-        return countSetBits(different_bits);
+        int result = x ^ y;
+        int diff=0;
+        while(result!=0) {
+            diff += result%2;
+            result = result>>1;
+        }
+        return diff;
     }
 };

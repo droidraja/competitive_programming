e0711796a1cf3f0d8a49598721a6eff99a565112
Raja <srajasudhan@gmail.com>
2023-04-09 04:14:51 +0000

    Sync LeetCode submission - Kids With the Greatest Number of Candies (cpp)

--- leetcode/problems/kids_with_the_greatest_number_of_candies/solution.cpp
+++ leetcode/problems/kids_with_the_greatest_number_of_candies/solution.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
-     
-        vector<bool> result;
-        
-        //Find the kid with max candies before distribution
-        int currentMax = 0;
-        for(auto candy:candies) {
-            if(candy>currentMax) currentMax=candy;    
-        }
-        
-        //For each kid check if all the extra candies are given to him, will he become the kid with the largest number of candies or match the existing kid with largest candies
-        for(auto candy:candies) {
-            if(candy+extraCandies>=currentMax) {
-                result.push_back(true);
-            } else {
-                result.push_back(false);
+        int max = 0;
+        for(int candy:candies) {
+            if(candy>max) {
+                max = candy;
             }
         }
-        
-        return result;
+        vector<bool> isMax;
+        for(int candy:candies) {
+            if(candy+extraCandies>=max) {
+                isMax.push_back(true);
+            } else {
+                isMax.push_back(false);
+            }
+        }
+        return isMax;
     }
 };

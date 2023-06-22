45e116f9ec4ed4cdf54a0ce5f41816f5fb4a0a3b
Raja <srajasudhan@gmail.com>
2023-06-22 15:34:05 +0000

    Sync LeetCode submission - Valid Parentheses (cpp)

--- leetcode/problems/valid_parentheses/solution.cpp
+++ leetcode/problems/valid_parentheses/solution.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> openBrackets;
         
-        for(char symbol:s) {
-            if(symbol==')' || symbol==']' || symbol=='}') {
-                if(openBrackets.size()==0) {
-                    return false;
-                }
-                char last = openBrackets.top();
-                openBrackets.pop();
-                char shouldBe;
-                switch(symbol) {
-                    case ')':
-                        shouldBe= '(';
-                        break;
-                    case '}':
-                        shouldBe = '{';
-                        break;        
-                    case ']':
-                        shouldBe = '[';
-                        break;
-                }
-                if(last!=shouldBe) {
-                    return false;
-                }
+        vector<char> myStack;
+        string opens = "({[";
+
+        for(auto ch:s) {
+            if(ch=='(' || ch=='{' || ch=='[') {
+                cout<<"pushing "<<ch<<endl;
+                myStack.push_back(ch);
             } else {
-                openBrackets.push(symbol);
+                if(myStack.size()==0) return false;
+                cout<<"Checking close of "<<ch<<" last char: "<<myStack[myStack.size()-1]<<endl;
+              if(myStack.size()==0) return false;
+              char last = myStack[myStack.size()-1];
+              bool matching=false;
+              switch(last) {
+                  case '(': matching = ch==')';break;
+                  case '{': matching = ch=='}';break;
+                  case '[': matching = ch==']';break;
+              }
+              cout<<"mathcin"<<matching<<endl;
+              if(!matching) return false; 
+              myStack.pop_back();
             }
         }
-        return openBrackets.size()==0;
-        
+
+        return myStack.size()==0;
     }
 };

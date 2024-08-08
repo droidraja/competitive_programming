0100872953955aed44bf7e0bf61e9fca90690515
Raja <srajasudhan@gmail.com>
2024-08-08 13:23:49 +0000

    Sync LeetCode submission - Integer to Roman (cpp)

--- leetcode/problems/integer_to_roman/solution.cpp
+++ leetcode/problems/integer_to_roman/solution.cpp
@@ -1,51 +1,51 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        string romans[] = {"I","V","X","L","C","D","M"};
-        
-        string finalResult = "";
-        int temp = num;
-        int index = 0;
-        while(temp!=0)
-        {
-            if(temp%10!=0)
-            {
-                int dec = temp%10;
-                if(dec<4)
-                {
-                    for(int i=0;i<temp%10;i++)
-                    {
-                        finalResult = romans[index]+finalResult;
-                    }
-                } else
-                if(dec==4)
-                {
-                    finalResult = romans[index]+romans[index+1]+finalResult;
-                } else
-                if(dec==5)
-                {
-                    finalResult = romans[index+1] +finalResult;
-                }
-                else
-                if(dec<9)
-                {
-                    
-                    for(int i=5;i<dec;i++)
-                    {
-                        finalResult = romans[index]+finalResult;
-                    }
-                    finalResult = romans[index+1] +finalResult;
-                }
-                else
-                {
-                    finalResult = romans[index] + romans[index+2] + finalResult;
-                }
-            }
-            
-            temp = temp/10;
-            index = index+2;
+    unordered_map<int,char> m;
+	m[1] = 'I'; 
+	m[5] = 'V'; 
+	m[10] = 'X'; 
+	m[50] = 'L'; 
+	m[100] = 'C'; 
+	m[500] = 'D'; 
+	m[1000] = 'M'; 
+
+
+    unordered_map<int,string> m2;
+    string roman = ""; 
+    while(num!=0) {
+        int temp  = num; int c=1;
+        while(temp>10) {temp/=10; c*=10;}
+        if(temp==4 || temp==9) {
+            roman+= m[c];
+            roman+= m[c*(temp+1)];
+            num -= temp*c;
+            continue;
         }
-        
-        return finalResult;
+
+        if(num>=1000) {
+            roman+=m[1000];
+            num-=1000;
+        } else if(num>=500) {
+            roman+=m[500];
+            num-=500;
+        } else if(num>=100) {
+            roman+=m[100];
+            num-=100;
+        } else if(num>=50) {
+            roman+=m[50];
+            num-=50;
+        } else if(num>=10) {
+            roman+=m[10];
+            num-=10;
+        } else if(num>=5) {
+            roman+=m[5];
+            num-=5;
+        } else {
+            roman+=m[1];
+            num-=1;
+        }
+    }
+    return roman;
     }
 };

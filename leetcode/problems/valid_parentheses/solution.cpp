e1f8f400a680a8c08a9ed33be915ae782cebc5d8
Raja <srajasudhan@gmail.com>
2024-08-12 17:48:15 +0000

    Sync LeetCode submission - Valid Parentheses (cpp)

--- leetcode/problems/valid_parentheses/solution.cpp
+++ leetcode/problems/valid_parentheses/solution.cpp
@@ -7,11 +7,9 @@ public:
 
         for(auto ch:s) {
             if(ch=='(' || ch=='{' || ch=='[') {
-                cout<<"pushing "<<ch<<endl;
                 myStack.push_back(ch);
             } else {
                 if(myStack.size()==0) return false;
-                cout<<"Checking close of "<<ch<<" last char: "<<myStack[myStack.size()-1]<<endl;
               if(myStack.size()==0) return false;
               char last = myStack[myStack.size()-1];
               bool matching=false;
@@ -20,7 +18,6 @@ public:
                   case '{': matching = ch=='}';break;
                   case '[': matching = ch==']';break;
               }
-              cout<<"mathcin"<<matching<<endl;
               if(!matching) return false; 
               myStack.pop_back();
             }

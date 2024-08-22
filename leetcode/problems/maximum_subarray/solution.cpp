7a9f0ac7aaacf8e2869811f8ce92e5424fa36981
Raja <srajasudhan@gmail.com>
2024-08-22 13:44:23 +0000

    Sync LeetCode submission - Maximum Subarray (cpp)

---
+++ leetcode/problems/maximum_subarray/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+      int max_so_far = INT_MIN;
+      int max_ending_here=0;
+
+      for(auto num:nums) {
+        max_ending_here += num;
+        if(max_ending_here>max_so_far) {
+            max_so_far = max_ending_here;
+        } 
+        if(max_ending_here<0) {
+            max_ending_here=0;
+        }
+      }
+      return max_so_far;  
+    }
+};
